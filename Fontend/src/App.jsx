1d1a5af85bd35b868db5f11ea5e44990015e7d71
Arvindkr-03 <arvind.bcastudent22.8111@cimage.in>
2024-12-31 21:44:04 +0530

    Successfully set up all required for creating this APP

---
+++ Fontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div><h1>Hello React users.</h1>
+      <h1>Happy New year_2025</h1>
+    
+      
+    </div>
+  )
+}
+
+export default App
